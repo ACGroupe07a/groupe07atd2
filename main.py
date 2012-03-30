f803ebf0ba98488ac663959cfac897cbbfbd172a
Arnaud Clement <arn.michaud@gmail.com>
2012-03-30 11:10:07 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from operators import add, nul
+from logic import and_. or_
+def main():
+        print "debut"
+        a=2
+        b=3
+        x=True
+        y=False
+        print add(a,b)
+        print nul(a,b)
+        print and_(a,b)
+        print or_(a,b)
+        print or_(x,y)
+
+if __name__ "" "__main__"
+        main()
